833e0bee85a53b48beeafa5f7091abfeec9d0098
Temitope <themyturpeokesanya@gmail.com>
2023-10-04 22:06:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+// const job = 'Frontend';
+// if(job === 'Accountant') {
+//     alert('victor is a frontend engineer!')
+// }else {
+//     alert('victor is not a tech bro')
+// }
+// console.log(job);
+// console.log(1+34+55);
+
+// values and variables
+// value
+// console.log('Victor');
+// console.log(20);
+// console.log(true);
+// variables
+// let myName = 'Victor';
+// let myAge = 18;
+// let isBasedInLagos =  false;
+
+// console.log(myName, myAge, isBasedInLagos);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+// console.log(myName);
+
+// data types
+// strings
+// let myName = 'Richard';
+// let myJob = "Frontend Engineer";
+// console.log(typeof myJob);
+// numbers
+// let myAge = 20;
+// let birthYear = 2003;
+// console.log(birthYear);
+// boolean
+// let isAMan = true;
+// console.log(isAMan);
+// undefine
+// let completedAssignment 
+// console.log(completedAssignment);
+
+// let, const and var
+// var
+// var myName = 'Temitope';
+// console.log(myName);
+// let
+// let myJob = 'frontend engineer';
+// myJob = 'designer';
+// console.log(myJob);
+// myName = 'Kayode';
+// console.log(myName);
+// const
+// const myHubby = 'baking';
+// console.log(myHubby);
+
+// basic operation
+// const currentYear = 2023;
+// const ayoadeBirthYear = 1997;
+// const ayodecurrentAge = currentYear - ayoadeBirthYear;
+// const kayodeBirthYear = 2002;
+// const kayodecurrentAge = currentYear - kayodeBirthYear;
+
+// console.log(ayodecurrentAge > kayodecurrentAge);
+// console.log(ayodecurrentAge , kayodecurrentAge);
+
+// challenge one
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 92;
+// const johnHeight = 1.95;
+// const markBMI = markWeight / markHeight ** 2;
+// const johnBMI = johnWeight / johnHeight ** 2;
+
+// console.log(markBMI, johnBMI);
+// const markHeigherBMI = markBMI > johnBMI;
+// console.log(markHeigherBMI);
+
+// string template literals
+// const firstName = 'Victor';
+// const job = 'Frontend Engineer';
+// const myLocation = 'warri';
+// const mobile = 9075648376;
+// const currentYear = 2023;
+// const myBirthYear = 1990;
+
+// const victor = "Hello there! My name is " + firstName + ". I'm a " + job + " I'm based in " + myLocation + ". I'm a " + (currentYear - myBirthYear) + "years old male. " + "You can reach me on " + mobile + ".";
+
+// console.log(victor);
+
+// const victor2 = `Hello there! My name is ${firstName}. I'm a ${job}. I'm based in ${myLocation}. I'm a ${currentYear - myBirthYear}years old male. You can reach me on ${mobile}.`
+// console.log(victor2);
+
+// if/else statement
+// challenge one
+// const markWeight = 78;
+// const markHeight = 1.69;
+// const johnWeight = 56;
+// const johnHeight = 1.29;
+// const markBMI = markWeight / markHeight ** 2;
+// const johnBMI = johnWeight / johnHeight ** 2;
+
+// console.log(markBMI, johnBMI);
+// const markHeigherBMI = markBMI > johnBMI;
+// console.log(markHeigherBMI);
+
+// if(condition){
+//    code you want to execute if true
+// }else{
+//    code you want to run if false 
+// }
+// if(markBMI > johnBMI){
+//     console.log(`Mark has a higher BMI of ${markBMI}`);
+// }else if(markBMI === johnBMI){
+//   console.log(`Mark and John have equal BMI`);
+// }else{
+//     console.log(`John has a higher BMI of ${johnBMI}`);
+// }
+
